874bd5bec25257968ceac97fe4b72f66477ec3ee
Sagar-Mazumder <sagarmazumder50@gmail.com>
2023-10-30 15:07:44 +0600

    react app created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+// imported for bootstrap 
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+
+  return (
+    <>
+      <h1 className='fw-bold'>ThinkEasy</h1>
+    </>
+  )
+}
+
+export default App
